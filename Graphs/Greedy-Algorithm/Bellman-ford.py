25ec30d63c343fd1ab5ff53adb98eed0767159bd
Dikshanta Chapagain <3108dikshanta@gmail.com>
2025-12-26 11:30:10 +0545

    bellman-ford algorithm added

---
+++ Graphs/Greedy-Algorithm/Bellman-ford.py
@@ -0,0 +1,24 @@
+def bellman_ford(edge, v, s):
+    dist = [float('inf')] * v
+    dist[s] = 0
+
+    for i in range(v - 1):
+        for j in range(len(edge)):
+            u = edge[j][0]
+            vtx = edge[j][1]
+            w = edge[j][2]
+
+            if dist[u] != float('inf') and dist[u] + w < dist[vtx]:
+                dist[vtx] = dist[u] + w
+
+    # negative cycle check
+    for j in range(len(edge)):
+        u = edge[j][0]
+        vtx = edge[j][1]
+        w = edge[j][2]
+
+        if dist[u] != float('inf') and dist[u] + w < dist[vtx]:
+            print("Negative cycle detected")
+            return None
+
+    return dist
